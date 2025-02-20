a55a689fd6af1cd089bb4b35a0fc30b6bcd2f7e8
Kojak <kojakeugenio@gmail.com>
2025-02-21 02:28:46 +0800

    f

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/ping")
+def ping():
+    return "pong", 200
 
 @app.route("/")
 def home():
     return render_template("index.html", title="Simple Flask App")
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
